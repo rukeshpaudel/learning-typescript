4b2ef22a2ab19cc6ebd3d7ecfa1cd33e32276e23
Rukesh Paudel <rukeshpaudel@gmail.com>
2022-07-05 11:30:12 +0545

    working with modules

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 "use strict";
+let age;
+age = 30;
+const userNamee = 'Rukesh';
+console.log(userNamee);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let age: number;
+age = 30;
+
+const userNamee = 'Rukesh'
+
+console.log(userNamee);
+
